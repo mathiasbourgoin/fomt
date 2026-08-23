025d30f592b37489c7d1555e8822d38214102940
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 11:25:02 +0200

    decomp: match func_08036530 NPC constructor

---
+++ src/code_08036530.cc
@@ -0,0 +1,17 @@
+#include "prelude.h"
+
+extern u8 gUnk_080F3010[];
+extern u32 vtable_unk_080E6CD8[];
+
+EC void *__10ANpcEntityP10GameObjectP3NpcUiPCvUiUiUi(
+    void *self, void *game_object, void *npc, u32 arg_3,
+    void const *arg_4, u32 anim_id, u32 arg_6, u32 arg_7);
+
+EC void *func_08036530(void *self, void *game_object, void *npc, u32 arg_3)
+{
+    __10ANpcEntityP10GameObjectP3NpcUiPCvUiUiUi(
+        self, game_object, npc, arg_3,
+        gUnk_080F3010, 0x320, 0x324, 0x3e6);
+    *(void **)((u8 *)self + 0x14) = vtable_unk_080E6CD8;
+    return self;
+}
