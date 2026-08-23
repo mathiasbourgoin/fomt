63e21fb30b3be8b7cfc8fad9c1991c2ea5ddf613
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 15:10:38 +0200

    decomp: match func_0803C00C forwarding wrapper

---
+++ src/code_0803C00C.cc
@@ -0,0 +1,8 @@
+#include "prelude.h"
+
+EC u32 func_0803C028(u32, u32, u32, u32, u32, u32, u32);
+
+EC u32 func_0803C00C(u32 a0, u32 a1, u32 a2, u32 a3, u32 a4, u32 a5, u32 a6)
+{
+    return func_0803C028(a0, a1, a2, a3, a4, a5, a6);
+}
