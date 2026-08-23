d1df4451649f9f57522136a7f8382884cdfc3fe3
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 14:11:57 +0200

    decomp: split more-items tail and match func_0800FFD0

---
+++ asm/more_items_tail.s
@@ -0,0 +1,82 @@
+    .INCLUDE "asm/macro.inc"
+    .SYNTAX UNIFIED
+
+    thumb_func_start func_0800FFD4
+func_0800FFD4: @ 0x0800FFD4
+    adds r0, #4
+    bx lr
+
+    thumb_func_start func_0800FFD8
+func_0800FFD8: @ 0x0800FFD8
+    ldr r1, [r0]
+    adds r1, #4
+    adds r0, r0, r1
+    bx lr
+
+    thumb_func_start func_0800FFE0
+func_0800FFE0: @ 0x0800FFE0
+    push {r4, lr}
+    adds r4, r0, #0
+    adds r1, #0x24
+    movs r2, #6
+    bl memcpy
+    adds r0, r4, #0
+    pop {r4}
+    pop {r1}
+    bx r1
+
+    thumb_func_start func_0800FFF4
+func_0800FFF4: @ 0x0800FFF4
+    push {lr}
+    adds r2, r0, #0
+    ldr r0, [r2]
+    cmp r0, #0x1d
+    bhi .L0801000E
+    adds r0, #4
+    adds r0, r2, r0
+    cmp r0, #0
+    beq .L08010008
+    strb r1, [r0]
+.L08010008:
+    ldr r0, [r2]
+    adds r0, #1
+    str r0, [r2]
+.L0801000E:
+    pop {r0}
+    bx r0
+    .align 2, 0
+
+    thumb_func_start func_08010014
+func_08010014: @ 0x08010014
+    push {lr}
+    adds r0, #0x24
+    movs r2, #6
+    bl memcpy
+    pop {r0}
+    bx r0
+    .align 2, 0
+
+    thumb_func_start func_08010024
+func_08010024: @ 0x08010024
+    push {r4, r5, lr}
+    adds r3, r0, #0
+    adds r5, r3, #4
+    ldr r0, [r3]
+    adds r0, #4
+    adds r0, r3, r0
+    adds r2, r0, #0
+    adds r1, r5, #0
+    ldr r4, [r3]
+    cmp r1, r2
+    beq .L08010040
+.L0801003A:
+    adds r1, #1
+    cmp r1, r2
+    bne .L0801003A
+.L08010040:
+    subs r0, r0, r5
+    subs r0, r4, r0
+    str r0, [r3]
+    pop {r4, r5}
+    pop {r0}
+    bx r0
---
+++ src/code_0800FFD0.cc
@@ -0,0 +1,6 @@
+#include "prelude.h"
+
+EC u32 func_0800FFD0(void *self)
+{
+    return *(u32 *)self;
+}
