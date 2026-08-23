c1ff9721f5c78e7b9533cf5c5f6a0b0f7c8ef9df
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 11:11:34 +0200

    decomp: match func_08036D68 NPC constructor

---
+++ src/code_08036D68.cc
@@ -0,0 +1,16 @@
+#include "prelude.h"
+
+extern u32 vtable_unk_080E6998[];
+
+EC void *__10ANpcEntityP10GameObjectP3NpcUiPCvUiUiUi(
+    void *self, void *game_object, void *npc, u32 arg_3,
+    void const *arg_4, u32 anim_id, u32 arg_6, u32 arg_7);
+
+EC void *func_08036D68(void *self, void *game_object, void *npc, u32 arg_3)
+{
+    __10ANpcEntityP10GameObjectP3NpcUiPCvUiUiUi(
+        self, game_object, npc, arg_3,
+        nullptr, 0x7fe, 0x7fe, 0);
+    *(void **)((u8 *)self + 0x14) = vtable_unk_080E6998;
+    return self;
+}
