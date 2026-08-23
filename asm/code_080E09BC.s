384579c71db46b766c06840adb6ec6494ef9160d
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 16:51:53 +0200

    decomp: match func_080E0A7C wrapper

--- asm/code_080E09BC.s
+++ asm/code_080E09BC.s
@@ -1,14 +1,6 @@
 	.INCLUDE "asm/macro.inc"
 	.SYNTAX UNIFIED
 
-	thumb_func_start func_080E0A7C
-func_080E0A7C: @ 0x080E0A7C
-	push {lr}
-	bl func_080098AC
-	pop {r0}
-	bx r0
-	.align 2, 0
-
 	thumb_func_start func_080E0A88
 func_080E0A88: @ 0x080E0A88
 	push {lr}
---
+++ src/code_080E0A7C.cc
@@ -0,0 +1,8 @@
+#include "prelude.h"
+
+EC void func_080098AC(void *self);
+
+EC void func_080E0A7C(void *self)
+{
+    func_080098AC(self);
+}
