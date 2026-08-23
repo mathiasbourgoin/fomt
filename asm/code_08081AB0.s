dbc976c8531e170713eee2c1479b3d7b6943bc2a
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 12:42:41 +0200

    decomp: match func_08081AB0 move helper

--- asm/code_08081AB0.s
+++ asm/code_08081AB0.s
@@ -1,22 +1,6 @@
 .INCLUDE "asm/macro.inc"
 .SYNTAX UNIFIED
 
-	thumb_func_start func_08081AB0
-func_08081AB0: @ 0x08081AB0
-	push {r4, r5, lr}
-	adds r5, r0, #0
-	adds r4, r1, #0
-	ldr r0, [r4, #4]
-	bl func_0808114C
-	ldr r1, [r4, #8]
-	movs r0, #0
-	str r0, [r4, #8]
-	str r1, [r5]
-	adds r0, r5, #0
-	pop {r4, r5}
-	pop {r1}
-	bx r1
-
 	thumb_func_start func_08081ACC
 func_08081ACC: @ 0x08081ACC
 	push {r4, lr}
