ec57b8a172f541d9085df6d54c8fdfc2fab12316
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 12:24:16 +0200

    decomp: match func_0809C0BC actor command

--- asm/code_actor_0809BFE8.s
+++ asm/code_actor_0809BFE8.s
@@ -1,15 +1,6 @@
     .INCLUDE "asm/macro.inc"
     .SYNTAX UNIFIED
 
-    thumb_func_start func_0809C0BC
-func_0809C0BC: @ 0x0809C0BC
-    ldr r1, [r1]
-    str r1, [r0, #0x10]
-    movs r1, #3
-    str r1, [r0, #0xc]
-    bx lr
-    .align 2, 0
-
     thumb_func_start func_0809C0C8
 func_0809C0C8: @ 0x0809C0C8
     ldr r1, [r1]
---
+++ src/code_0809C0BC.cc
@@ -0,0 +1,7 @@
+#include "prelude.h"
+
+EC void func_0809C0BC(void *self, void const *value)
+{
+    *(u32 *)((u8 *)self + 0x10) = *(u32 const *)value;
+    *(u32 *)((u8 *)self + 0xc) = 3;
+}
