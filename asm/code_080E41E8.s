96eaef014130fda50a686ec0b719bd2fd9d17f4e
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 14:48:41 +0200

    decomp: match func_080E41E8 child release destructor

--- asm/code_080E41E8.s
+++ asm/code_080E41E8.s
@@ -1,28 +1,6 @@
 .INCLUDE "asm/macro.inc"
 .SYNTAX UNIFIED
 
-	thumb_func_start func_080E41E8
-func_080E41E8: @ 0x080E41E8
-	push {r4, r5, lr}
-	adds r4, r0, #0
-	adds r5, r1, #0
-	ldr r1, [r4, #4]
-	cmp r1, #0
-	beq .L080E4200
-	ldr r0, [r1]
-	ldr r2, [r0, #8]
-	adds r0, r1, #0
-	movs r1, #3
-	bl _call_via_r2
-.L080E4200:
-	adds r0, r4, #0
-	adds r1, r5, #0
-	bl func_080007EC
-	pop {r4, r5}
-	pop {r0}
-	bx r0
-	.align 2, 0
-
 	thumb_func_start func_080E4210
 func_080E4210: @ 0x080E4210
 	push {r4, r5, lr}
@@ -393,4 +371,3 @@ func_080E4438: @ 0x080E4438
 	pop {r1}
 	bx r1
 	.align 2, 0
-
