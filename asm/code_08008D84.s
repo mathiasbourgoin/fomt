d33c0f7a6fc46ab27d3835c4a61049582bb84401
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 12:04:11 +0200

    decomp: match func_08008D9C DMA status

--- asm/code_08008D84.s
+++ asm/code_08008D84.s
@@ -1,15 +1,6 @@
     .INCLUDE "asm/macro.inc"
     .SYNTAX UNIFIED
 
-    thumb_func_start func_08008D9C
-func_08008D9C: @ 0x08008D9C
-    ldr r0, .L08008DA4 @ =0x040000C4
-    ldr r0, [r0]
-    lsrs r0, r0, #0x1f
-    bx lr
-    .align 2, 0
-.L08008DA4: .4byte 0x040000C4
-
     thumb_func_start func_08008DA8
 func_08008DA8: @ 0x08008DA8
     push {lr}
---
+++ src/code_08008D9C.cc
@@ -0,0 +1,6 @@
+#include "prelude.h"
+
+EC u32 func_08008D9C()
+{
+    return *(volatile u32 *)0x040000c4 >> 31;
+}
