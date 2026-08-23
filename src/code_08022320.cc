6000b971473d736b05c821a0b0c816910557b501
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 15:08:57 +0200

    decomp: match func_08022320 value mapping

---
+++ src/code_08022320.cc
@@ -0,0 +1,13 @@
+#include "prelude.h"
+
+EC int func_08022320(int value)
+{
+    switch (value) {
+    case 0:
+        return 1;
+    case 1:
+        return 2;
+    default:
+        return 1;
+    }
+}
