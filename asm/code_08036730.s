d72123a9d896679900f6e357b408c84bd3ead437
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 13:27:31 +0200

    decomp: match func_080367AC conditional wrapper

--- asm/code_08036730.s
+++ asm/code_08036730.s
@@ -33,19 +33,3 @@ func_08036768: @ 0x08036768
 .L080367A0: .4byte 0x0000027B
 .L080367A4: .4byte 0x00002181
 .L080367A8: .4byte 0x0000027F
-
-    thumb_func_start func_080367AC
-func_080367AC: @ 0x080367AC
-    push {r4, r5, lr}
-    adds r5, r0, #0
-    adds r4, r1, #0
-    bl func_08034F00
-    cmp r4, #1
-    ble .L080367C0
-    adds r0, r5, #0
-    bl func_08036768
-.L080367C0:
-    pop {r4, r5}
-    pop {r0}
-    bx r0
-    .align 2, 0
