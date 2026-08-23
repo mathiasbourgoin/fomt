1d8f60d9236d6e71f440f947c8aae4273de35973
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 12:39:14 +0200

    decomp: match func_080ADD20 lookup helpers

--- asm/code_080ADD20.s
+++ asm/code_080ADD20.s
@@ -1,39 +1,6 @@
 	.INCLUDE "asm/macro.inc"
 	.SYNTAX UNIFIED
 
-	thumb_func_start func_080ADD20
-func_080ADD20: @ 0x080ADD20
-	push {lr}
-	sub sp, #0x30
-	ldr r1, .L080ADD3C @ =gUnk_0852D984
-	mov r0, sp
-	bl func_0805E6CC
-	mov r0, sp
-	ldrh r0, [r0, #0x20]
-	ldr r1, .L080ADD40 @ =vtable_unk_080E5A28
-	str r1, [sp]
-	add sp, #0x30
-	pop {r1}
-	bx r1
-	.align 2, 0
-.L080ADD3C: .4byte gUnk_0852D984
-.L080ADD40: .4byte vtable_unk_080E5A28
-
-	thumb_func_start func_080ADD44
-func_080ADD44: @ 0x080ADD44
-	movs r2, #0
-	str r2, [r0]
-	movs r3, #0
-	movs r1, #0x80
-	lsls r1, r1, #1
-	strh r1, [r0, #4]
-	strh r2, [r0, #6]
-	str r2, [r0, #8]
-	str r2, [r0, #0xc]
-	str r2, [r0, #0x10]
-	strb r3, [r0, #0x14]
-	bx lr
-
 	thumb_func_start func_080ADD5C
 func_080ADD5C: @ 0x080ADD5C
 	str r1, [r0, #8]
---
+++ src/code_080ADD20.cc
@@ -0,0 +1,45 @@
+#include "prelude.h"
+
+struct Lookup
+{
+    void *vtable;
+    u8 pad_4[0x1C];
+    u16 value;
+    u8 pad_22[0xE];
+};
+
+struct TextCursor
+{
+    u32 field_0;
+    u16 field_4;
+    u16 field_6;
+    void *field_8;
+    void *field_C;
+    void *field_10;
+    u8 field_14;
+};
+
+extern u8 gUnk_0852D984[];
+extern u32 vtable_unk_080E5A28[];
+
+EC void func_0805E6CC(Lookup *self, const void *data);
+
+EC u16 func_080ADD20()
+{
+    Lookup lookup;
+    func_0805E6CC(&lookup, gUnk_0852D984);
+    u16 value = lookup.value;
+    lookup.vtable = vtable_unk_080E5A28;
+    return value;
+}
+
+EC void func_080ADD44(TextCursor *self)
+{
+    self->field_0 = 0;
+    self->field_4 = 0x100;
+    self->field_6 = 0;
+    self->field_8 = nullptr;
+    self->field_C = nullptr;
+    self->field_10 = nullptr;
+    self->field_14 = 0;
+}
