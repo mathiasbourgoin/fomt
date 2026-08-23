420b31ba708be45c13e519f988fee099d6d64010
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 15:27:35 +0200

    decomp: match func_0805FD44 ownership move

--- asm/code_0805FD44.s
+++ asm/code_0805FD44.s
@@ -1,34 +1,6 @@
 	.INCLUDE "asm/macro.inc"
 	.SYNTAX UNIFIED
 
-	thumb_func_start func_0805FD44
-func_0805FD44: @ 0x0805FD44
-	push {r4, r5, lr}
-	sub sp, #4
-	adds r5, r0, #0
-	adds r4, r1, #0
-	ldr r1, [r4, #4]
-	mov r0, sp
-	bl func_0805EE44
-	ldr r1, [sp]
-	cmp r1, #0
-	beq .L0805FD66
-	ldr r0, [r1]
-	ldr r2, [r0, #8]
-	adds r0, r1, #0
-	movs r1, #3
-	bl _call_via_r2
-.L0805FD66:
-	ldr r1, [r4, #8]
-	movs r0, #0
-	str r0, [r4, #8]
-	str r1, [r5]
-	adds r0, r5, #0
-	add sp, #4
-	pop {r4, r5}
-	pop {r1}
-	bx r1
-
 	thumb_func_start func_0805FD78
 func_0805FD78: @ 0x0805FD78
 	push {r4, r5, r6, lr}
@@ -21618,4 +21590,3 @@ func_08069D84: @ 0x08069D84
 	pop {r4, r5, r6, r7}
 	pop {r0}
 	bx r0
-
