aede3a44b1173813d7c25a07cfdcf5bf9ca3978b
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-08-23 18:20:55 +0200

    decomp: match AD938 wrapper family

---
+++ src/code_080AD938.cc
@@ -0,0 +1,29 @@
+#include "prelude.h"
+
+EC void func_080CFFC0(void *);
+EC void func_080A63B8(void *);
+EC void func_080A635C(void *);
+EC void func_080A6300(void *);
+EC void func_080A6024(void *);
+EC void func_080A607C(void *);
+EC void func_080A62BC(void *);
+EC void func_080A6278(void *);
+EC void func_080A6234(void *);
+EC void func_080A61F0(void *);
+EC void func_080A6194(void *);
+EC void func_080A6138(void *);
+EC void func_080A601C(void *);
+
+EC void func_080AD938(u8 *self) { func_080CFFC0(*(void **)(self + 4)); }
+EC void func_080AD944(u8 *self) { func_080A63B8(*(void **)(self + 4)); }
+EC u32 func_080AD950(u8 *self) { func_080A635C(*(void **)(self + 4)); return 0; }
+EC u32 func_080AD960(u8 *self) { func_080A6300(*(void **)(self + 4)); return 0; }
+EC void func_080AD970(u8 *self) { func_080A6024(*(void **)(self + 4)); }
+EC void func_080AD97C(u8 *self) { func_080A607C(*(void **)(self + 4)); }
+EC void func_080AD988(u8 *self) { func_080A62BC(*(void **)(self + 4)); }
+EC void func_080AD994(u8 *self) { func_080A6278(*(void **)(self + 4)); }
+EC void func_080AD9A0(u8 *self) { func_080A6234(*(void **)(self + 4)); }
+EC void func_080AD9AC(u8 *self) { func_080A61F0(*(void **)(self + 4)); }
+EC void func_080AD9B8(u8 *self) { func_080A6194(*(void **)(self + 4)); }
+EC void func_080AD9C4(u8 *self) { func_080A6138(*(void **)(self + 4)); }
+EC void func_080AD9D0(u8 *self) { func_080A601C(*(void **)(self + 4)); }
